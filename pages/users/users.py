55a866db86a014ecca3f8e781b82ef8f90b46b37
Matt Stark <linuxisawesome@gmail.com>
2013-01-31 22:10:24 +1100

    Made it so the backend for admin user views works properly and changes the user permissions

--- pages/users/users.py
+++ pages/users/users.py
@@ -5,4 +5,9 @@ from . import render
 @require_admin
 def users(response, user):
     teachers = Users().get_teachers()
+    for teacher in teachers:
+        newstate = response.get_field("level{}".format(teacher.id))
+        if newstate != None and newstate.isdigit() and 2 < int(newstate) < 6:
+            teacher.state = int(newstate)
+            teacher.save()
     render("users/manage.html", response, {"teachers": teachers})
