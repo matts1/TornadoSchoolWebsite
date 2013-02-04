bf34c97802155d1b4573997932d6a71fbaca2584
Matt Stark <linuxisawesome@gmail.com>
2013-02-04 19:56:13 +1100

    Fixed a few bugs on the user management page. Admin can now change the permissions of themselves.

--- pages/users/users.py
+++ pages/users/users.py
@@ -1,5 +1,7 @@
 from functions.users import require_admin
 from models.tables.users import Users
+from models.tables.sessions import Sessions
+from models.rows.users import User
 from . import render
 
 @require_admin
@@ -8,6 +10,11 @@ def users(response, user):
     for teacher in teachers:
         newstate = response.get_field("level{}".format(teacher.id))
         if newstate != None and newstate.isdigit() and 2 < int(newstate) < 6:
+            if int(newstate) < teacher.state:
+                Sessions().deleteuser(teacher.id)
             teacher.state = int(newstate)
             teacher.save()
+    user.discard()
+    if user.state != 5:
+        response.redirect("/login")
     render("users/manage.html", response, {"teachers": teachers})
